68999e36545a9a01ebc98ea60912b8c5fb573c9e
park chan <parkchan428@gmail.com>
2018-02-02 16:26:33 +0900

    detail.html에 투표가능한 form을 구현

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,4 +1,3 @@
-from django.template import loader
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 
@@ -8,29 +7,49 @@ from .models import Question
 def index(request):
     # 가장 최근에 발행된 최대 5개의 Question목록
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # 쉼표단위로 구분된 Question목록의 각 항목의 question_text로 만들어진 문자열
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-
     context = {
         'latest_question_list': latest_question_list,
     }
-    # return render(request, 'polls/index.html', context)
 
-    template = loader.get_template('polls/index.html')
-    return HttpResponse(template.render(context, request))
+    # render를 사용
+    return render(request, 'polls/index.html', context)
+
+    # template을 명시적으로 불러와 rendering
+    # template = loader.get_template('poll/index.html')
+    # return HttpResponse(template.render(context, request))
+
+    # 쉼표단위로 구분된 Question목록의
+    #   각 항목의 question_text로 만들어진 문자열을 리턴
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
 
 
 def detail(request, question_id):
+    """
+    question_id에 해당하는 Question객체를 템플릿에 전달
+        key: "question"
+        template: polls/templates/polls/detail.html
+    템플릿에서는
+    1. 전달받은 question의 question_text를 출력
+        {{ question.question_text }}
+    2. question이 가진 모든 Choice들을 ul > li로 출력
+        {% for choice in question.choice_set.all %}
+    :param request:
+    :param question_id:
+    :return:
+    """
     # try:
+    #     # question을 get()시도
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
+    #     # DoesNotExist예외가 발생 시
+    #     # Http404에러를 발생시키면서 메시지를 전달
     #     raise Http404('Question does not exist')
     question = get_object_or_404(Question, pk=question_id)
     context = {
         'question': question,
     }
-    return HttpResponse("You're looking at question %s." % question_id)
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
@@ -39,4 +58,5 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
+    choice = request.POST('choice')
     return HttpResponse("You're voting on question %s." % question_id)
