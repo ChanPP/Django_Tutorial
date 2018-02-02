bf06892c9d8bb0b2f17bd27d0aad5b825294bf22
park chan <parkchan428@gmail.com>
2018-02-02 16:48:56 +0900

    results view, template구현

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Question, Choice
@@ -53,16 +53,44 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    """
+    question_id에 해당하는 Question객체 한개를 넘김
+    polls/results.html에서
+    question에 속한 Choice들의 목록을 보여주면서
+        각 항목의 votes값을 출력
+    :param request:
+    :param question_id:
+    :return:
+    """
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/results.html', context)
 
 
 def vote(request, question_id):
+    """
+    1. request.POST의 choice키에 온 value를
+        pk로 갖는 Choice객체의
+        votes값을 1 증가시키고
+        DB에 저장
+    2. 이후 question_id에 해당하는 results뷰로 redirect
+    :param request:
+    :param question_id:
+    :return:
+    """
+    # request.POST의 name(choice)에 온 value를
     choice_pk = request.POST['choice']
+    # pk로 가지는 Choice객체 choice
     choice = Choice.objects.get(pk=choice_pk)
+    # choice의 votes값을 1 증가시키고
     choice.votes += 1
+    # DB에 저장
     choice.save()
 
+    # 이후 question_id인자값을 전달하며
+    # polls:results로 redirect
     return redirect(
         'polls:results',
         question_id=question_id
