87077a2d4bdf6d87f195cb7680837eb9d1843f7e
park chan <parkchan428@gmail.com>
2018-02-02 15:19:41 +0900

    index view가 템플릿을 사용하도록 변경

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
-    # re_path를 사용해서 정규표현식으로 구현
-    re_path(r'^$', views.index, name="index"),
-    re_path(r'^(?P<question_id>\d+)$', views.detail, name="detail"),
-    re_path(r'^(?P<question_id>\d+)/result/$', views.result, name="result"),
-    re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name="vote"),
-
+    # re_path를 사용해서 정규표현식으로 구현해보세요
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 ]
--- django/polls/views.py
+++ django/polls/views.py
@@ -1,5 +1,39 @@
+from django.template import loader
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 가장 최근에 발행된 최대 5개의 Question목록
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # 쉼표단위로 구분된 Question목록의 각 항목의 question_text로 만들어진 문자열
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return render(request, 'polls/index.html', context)
+
+    template = loader.get_template('polls/index.html')
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+
+
+
+
+
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
